f47d3518a0ff64fe89fbd3c2da88c243e9b5f36e
papstuc <papst1337@protonmail.com>
2023-03-28 20:48:39 +0200

    added debug console

---
+++ counterstrike2/counterstrike2/utilities/debug_console/debug.cpp
@@ -0,0 +1,34 @@
+#include <Windows.h>
+#include <cstdio>
+
+#include "debug.hpp"
+
+void debug::initialize(const wchar_t* console_title)
+{
+	AllocConsole();
+
+	freopen_s(reinterpret_cast<_iobuf**>(__acrt_iob_func(0)), "conin$", "r", static_cast<_iobuf*>(__acrt_iob_func(0)));
+	freopen_s(reinterpret_cast<_iobuf**>(__acrt_iob_func(1)), "conout$", "w", static_cast<_iobuf*>(__acrt_iob_func(1)));
+	freopen_s(reinterpret_cast<_iobuf**>(__acrt_iob_func(2)), "conout$", "w", static_cast<_iobuf*>(__acrt_iob_func(2)));
+
+	SetConsoleTitle(console_title);
+}
+
+void debug::release()
+{
+	fclose(static_cast<_iobuf*>(__acrt_iob_func(0)));
+	fclose(static_cast<_iobuf*>(__acrt_iob_func(1)));
+	fclose(static_cast<_iobuf*>(__acrt_iob_func(2)));
+
+	FreeConsole();
+}
+
+void debug::log(const wchar_t* message...)
+{
+	wprintf_s(message);
+}
+
+void debug::log(const char* message...)
+{
+	printf_s(message);
+}
